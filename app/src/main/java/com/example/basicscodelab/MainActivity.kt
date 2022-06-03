490b4ad81a8c56a204c9a875adb35e053df9e095
조엘_전국진 <joel@afreecatv.com>
2022-06-04 00:57:22 +0900

    UI 조정

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +28,9 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun Greeting(name: String) {
-        Text(text = "Hello $name!")
+        Surface(color = MaterialTheme.colors.primary) {
+            Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+        }
     }
 
     @Preview(showBackground = true, name = "Text preview")
