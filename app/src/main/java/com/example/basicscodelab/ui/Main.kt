f80ecd0cbed20dc3cb6c95ccd5dd493727e0951c
조엘_전국진 <joel@afreecatv.com>
2022-06-04 03:30:58 +0900

    상태 유지

--- app/src/main/java/com/example/basicscodelab/ui/Main.kt
+++ app/src/main/java/com/example/basicscodelab/ui/Main.kt
@@ -3,12 +3,12 @@ package com.example.basicscodelab.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 
 @Composable
 fun Main() {
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnBoarding) {
         OnboardingScreen {
